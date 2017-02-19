7151cf1f293da3387daa2814487afb8dc26555b2
Murdho <murdho@murdho.com>
2017-02-19 08:53:27 +0200

    Move instruction execution from Instruction to Schedule for better self

--- lib/payment_schedule/instruction.rb
+++ lib/payment_schedule/instruction.rb
@@ -5,41 +5,11 @@ module PaymentSchedule
     attr_accessor :required_input
     attr_accessor :helpers
     attr_accessor :components
-    attr_accessor :helper_memory
-    attr_accessor :component_memory
 
     def initialize
-      self.required_input   = []
-      self.helpers          = {}
-      self.components       = {}
-      self.helper_memory    = {}
-      self.component_memory = {}
-    end
-
-    def [](name, row_no = nil)
-      row_no ? component_get(name, row_no) : helper_get(name)
-    end
-
-    def component_get(name, row_no)
-      return unless components.key?(name)
-
-      memory_key     = [name, row_no].join(':')
-      memoized_value = component_memory[memory_key]
-      return memoized_value if memoized_value
-
-      component_memory[memory_key] = instance_exec(
-        row_no,
-        &components[name][row_no]
-      )
-    end
-
-    def helper_get(name)
-      return unless helpers.key?(name)
-
-      memoized_value = helper_memory[name]
-      return memoized_value if memoized_value
-
-      helper_memory[name] = instance_exec(&helpers[name])
+      self.required_input = []
+      self.helpers        = {}
+      self.components     = {}
     end
 
     def require_input(*keys)
--- lib/payment_schedule/schedule.rb
+++ lib/payment_schedule/schedule.rb
@@ -7,20 +7,60 @@ module PaymentSchedule
     end
 
     attr_accessor :input
-
-    # TODO: input validation
+    attr_accessor :helper_memory
+    attr_accessor :component_memory
 
     def initialize(**input)
-      self.input = input
+      self.input            = input
+      self.helper_memory    = {}
+      self.component_memory = {}
 
       validate_input!
     end
 
-    def [](*args)
-      input[args.first] || instruction[*args]
+    def [](name, row_no = nil)
+      if row_no
+        component_get(name, row_no)
+      else
+        input[name] || helper_get(name)
+      end
     end
 
     private
+
+    def instruction
+      self.class.instruction
+    end
+
+    def components
+      instruction.components
+    end
+
+    def helpers
+      instruction.helpers
+    end
+
+    def component_get(name, row_no)
+      return unless components.key?(name)
+
+      memory_key     = [name, row_no].join(':')
+      memoized_value = component_memory[memory_key]
+      return memoized_value if memoized_value
+
+      component_memory[memory_key] = instance_exec(
+        row_no,
+        &components[name][row_no]
+      )
+    end
+
+    def helper_get(name)
+      return unless helpers.key?(name)
+
+      memoized_value = helper_memory[name]
+      return memoized_value if memoized_value
+
+      helper_memory[name] = instance_exec(&helpers[name])
+    end
 
     def validate_input!
       missing_keys = instruction.required_input.reject do |required_key|
@@ -30,10 +70,6 @@ module PaymentSchedule
       return if missing_keys.empty?
 
       raise Error, "Missing required input key(s) :#{missing_keys.join(', :')}"
-    end
-
-    def instruction
-      self.class.instruction
     end
   end
 end
